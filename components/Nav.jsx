b4443564a4ef0c5504f2c3c112e752254b7906bd
dudun <dudunoyume@gmail.com>
2023-03-15 18:48:00 +0800

    nav bar design

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -13,20 +13,40 @@ export default function Fanciblock() {
           alt="Meow"
           quality={75}
         />
+
         貓貓大踏查
+        
       </Link>
+
+      {/* <label className={styles.dropdownController} htmlFor="rwdCheck" ><i className="fas fa-arrow-down-short-wide" href="#"></i> </label> */}
+      <input id ="rwdCheck" type="checkbox" name="rwdCheck" className={styles.rwdCheck}/>
+      
+
+
       <nav className={styles.mainNav}>
-        <Link href="#"> 計畫介紹 </Link>
-        <Link href="#"> 喵星人世界 </Link>
-        <Link href="#"> 水世界 </Link>
-        <Link href="#"> RWD原來是這樣 </Link>
-      </nav>
+        <div className={styles.subNav}>
+          <Link href="#"> 計畫介紹 </Link>
+          <Link href="#"> 喵星人世界 </Link>
+          <Link href="#"> 水世界 </Link>
+          <Link href="#"> 時間軸 </Link>
+
+        </div>
+
+
       <form className={styles.headerSearch}>
         <input type="search" name="" id="" />
-        <button>
+        <button>    
           <i className="fab fa-sistrix"></i>
         </button>
       </form>
+      
+    </nav>
+
+
+
+
+
+
     </div>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Layout from 'components/Layout'
-
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Layout from "components/Layout";
 
 export default function Home() {
   return (
     <Layout returnBack={true}>
-<div>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam magnam voluptate accusantium dignissimos, voluptatibus neque quo recusandae, provident culpa distinctio vero, iure quos. Omnis, explicabo. Obcaecati fuga illum perspiciatis. Amet!
-
-</div>
-
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam magnam
+        voluptate accusantium dignissimos, voluptatibus neque quo recusandae,
+        provident culpa distinctio vero, iure quos. Omnis, explicabo. Obcaecati
+        fuga illum perspiciatis. Amet!
+      </div>
     </Layout>
-
- 
-  )
+  );
 }
